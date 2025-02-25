c1664d9fa58aa467178bd40e4835cf74df70a331
bálxzzy <balzz9a@gmail.com>
2025-02-25 16:18:22 +0800

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     return (
         <footer className="text-center mt-6 text-sm text-gray-400">
             <hr className="h-px bg-gray-800 my-4 border-0"/>
-            <p>See what I'm currently listening on Spotify</p>
             <div className="flex justify-center items-center gap-2 mt-1">
                 <PiStarFourLight className="text-gray-500"/>
                 <p>bálzz • I Think Yt Short Is Good.</p>
